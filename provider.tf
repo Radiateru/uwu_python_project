ed5502d2d941a9bf7b854c91adcde04e3528a4c6
Radiateru <ant.cariou@gmail.com>
2026-03-20 11:04:22 +0100

    modified:   variable.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,6 @@ terraform {
   }
   backend "s3" {
     bucket = "bigbucket-bigrecharge"
-    key    = "dev/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
   }
--- variable.tf
+++ variable.tf
@@ -2,4 +2,7 @@ variable "instance_type" {
   type = string
   description = "La recharge"
   default = "t2.micro"
-}       
+}
+variable "environment" {
+  type = string
+}
